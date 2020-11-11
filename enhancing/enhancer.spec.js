70a22df4101d4a074cbfd0de2d2b4bb6bfc62cd8
Joshua Whitwell <josh.whitwell@gmail.com>
2020-11-11 17:08:34 -0500

    added repair method test suite

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -18,4 +18,44 @@ describe('set up', () => {
     })
 })
 
+describe('repair method', () => {
+    const testItem = { name: 'hammer', durability: 5, enhancement: 15 } 
+    const repairOutput = enhancer.repair(testItem)
 
+    it('method is defined', () => {
+        expect(enhancer.repair).toBeDefined()
+    })
+
+    it('returns obj', () => {
+        expect(repairOutput).toBeInstanceOf(Object)
+    })
+
+    it('returns new obj', () => {
+        expect(repairOutput).not.toBe(testItem)
+    })
+    
+    it('returns obj with correct key value pairs', () =>{
+        expect(repairOutput).toHaveProperty('name', 'hammer')
+        expect(repairOutput).toHaveProperty('durability', 100)
+        expect(repairOutput).toHaveProperty('enhancement', 15)
+    })
+
+    it('name is of type string', () => {
+        expect(typeof repairOutput.name).toBe('string')
+    })
+
+    it('durability and enhancement are of type number', () => {
+        expect(typeof repairOutput.durability).toBe('number')
+        expect(typeof repairOutput.enhancement).toBe('number')
+    })
+
+    it('sets durability to 100', () => {
+        expect(repairOutput.durability).toBe(100)
+        expect(repairOutput.name).toEqual(testItem.name)
+        expect(repairOutput.enhancement).toEqual(testItem.enhancement)
+    })
+})
+
+describe('success method', () => {
+    
+})
