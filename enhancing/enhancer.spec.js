aec8f4aeff97141b110d8807dddee030dd9c41b5
Joshua Whitwell <josh.whitwell@gmail.com>
2020-11-11 15:04:27 -0500

    installs jest, adds set up test suite

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,21 @@
 const enhancer = require('./enhancer.js');
-// test away!
+
+describe('set up', () => {
+    it('sanity checks', () => {
+        expect(1 + 1).toBe(2)
+        expect(1 + 1).not.toBe(3)
+        expect(true).toBe(true)
+        expect(false).not.toBe(true)
+        expect([]).toBeTruthy()
+        expect('').not.toBeTruthy()
+        expect(5).toBeLessThan(6)
+        expect(5).toBeGreaterThan(4)
+        expect(undefined).not.toBeDefined()
+        expect(null).toBeNull()
+        expect(NaN).toBeNaN()
+        expect({}).not.toBe({})
+        expect({}).toEqual({})
+    })
+})
+
+
