9704be82404a5f5f34063d714230661574ba192a
Joshua Whitwell <josh.whitwell@gmail.com>
2020-11-11 17:55:52 -0500

    adds success method suite and failure method suite

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,11 +6,21 @@ module.exports = {
 };
 
 function success(item) {
-  return { ...item };
+  if(item.enhancement < 20) {
+    return { ...item, enhancement: item.enhancement + 1 };
+  } else if (item.enhancement === 20) {
+    return { ...item }
+  }
 }
 
 function fail(item) {
-  return { ...item };
+  if (item.enhancement < 15) {
+    return { ...item, durability: item.durability - 5 };
+  } else if (item.enhancement >= 15 && item.enhancement <= 16) {
+    return { ...item, durability: item.durability - 10 }
+  } else if (item.enhancement > 16) {
+    return { ...item, durability: item.durability - 1 }
+  }
 }
 
 function repair(item) {
